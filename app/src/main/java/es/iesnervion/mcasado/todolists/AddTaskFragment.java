9e5bf0a03183c22d04fc84e24a26ef5e2546d9fc
Miguel <miguel.casado@iesnervion.es>
2021-02-14 21:11:50 +0100

    First Commit.

---
+++ app/src/main/java/es/iesnervion/mcasado/todolists/AddTaskFragment.java
@@ -0,0 +1,34 @@
+package es.iesnervion.mcasado.todolists;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddTaskFragment extends Fragment {
+
+
+    public AddTaskFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_task, container, false);
+    }
+}
