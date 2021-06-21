db986818011e918c028826bfbddfa4c1610d776c
Miguel <miguel.casado@iesnervion.es>
2021-06-21 23:36:39 +0200

    Added Navigation Drawer

--- app/src/main/java/es/iesnervion/mcasado/todolists/MainActivity.java
+++ app/src/main/java/es/iesnervion/mcasado/todolists/MainActivity.java
@@ -1,15 +1,25 @@
 package es.iesnervion.mcasado.todolists;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     AddEditTaskViewModel viewModel;
+    private DrawerLayout drawer;
+    MaterialToolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
                                                         .findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
 
+        // DrawerNavigation & toolbar
+        drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        boolean res = super.onOptionsItemSelected(item);
+        if (item.getItemId()==android.R.id.home){
+            drawer.openDrawer(GravityCompat.START,true);
+            res = true;
+        }
+        return res;
+    }
 }
